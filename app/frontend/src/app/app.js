f8311a28b635eda66a73e0fb95e5e7519493e022
Gaston Robledo <gaston.robledo@santexgroup.com>
2015-01-26 15:06:39 -0300

    update changes

--- app/frontend/src/app/app.js
+++ app/frontend/src/app/app.js
@@ -11,7 +11,12 @@
         interpolate.endSymbol(']>');
         growlProvider.globalTimeToLive(5000);
         growlProvider.onlyUniqueMessages(true);
-        themeProvider.setDefaultTheme('blue');
+        themeProvider.theme('default')
+            .primaryColor('blue')
+            .accentColor('green');
+        themeProvider.theme('alternative')
+            .primaryColor('orange')
+            .accentColor('pink');
     }
 
 
